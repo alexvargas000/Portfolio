9c128e90adf99ed62c60b55a7072263f282ccc9d
alexvargas000 <75872850+alexvargas000@users.noreply.github.com>
2023-03-21 14:42:24 +0100

    Added Scrolling animations

--- JS/test.js
+++ JS/test.js
@@ -1,8 +1,13 @@
 var menu = document.getElementById('Works-Menu');
 
 function openSub(){
+    var menuWordPosition = document.body.getBoundingClientRect().right - document.getElementById('sub-word').getBoundingClientRect().right;
+
     menu.style.height = '9.5em';
     menu.style.transition = 'height 450ms ease-in-out';
+
+    menu.style.right = menuWordPosition.right + 'px'; 
+    console.log(menuWordPosition.right + 'px')
 }
 
 function closeSub(){
