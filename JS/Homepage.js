127ca0bcbe40b6348bf126a65f717bb0b62b5160
alexvargas000 <75872850+alexvargas000@users.noreply.github.com>
2024-01-08 18:38:53 -0500

    Cleaned up and added transitions

---
+++ JS/Homepage.js
@@ -0,0 +1,21 @@
+menuItem = $(".Menu-item");
+
+$(menuItem).mouseenter(function(){
+
+    $(this).css('width', '26em');
+});
+
+$(menuItem).mouseleave(function(){
+
+    $(this).css('width', '25em');
+});
+
+$('#Bloopers').mouseenter(function(){
+
+    $(this).css('width', '52em');
+});
+
+$("#Bloopers").mouseleave(function(){
+
+    $(this).css('width', '50em');
+});
