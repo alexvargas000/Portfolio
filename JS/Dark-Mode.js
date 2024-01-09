3b95a2a7bee3dc38a71bde32cf6ae395ab7fc5dc
alexvargas000 <75872850+alexvargas000@users.noreply.github.com>
2024-01-09 17:25:03 -0500

    Added Description to Project Page

--- JS/Dark-Mode.js
+++ JS/Dark-Mode.js
@@ -4,6 +4,7 @@ $("#dark-mode-button").click(function(){
 
     if(darkModeOn == false){
         $("*").css('background-color', '#0e0c1c');
+        $("*").css('transition', 'all 1s');
         $("#Headings").css('background-color', 'transparent');
         $("#Header-Links").css('background-color', 'transparent');
         $("li").css('background-color', 'transparent');
