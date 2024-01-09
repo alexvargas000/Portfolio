34c612c0d42d276ba2f2b8b33676e08a0c1728f0
alexvargas000 <75872850+alexvargas000@users.noreply.github.com>
2024-01-09 15:21:51 -0500

    Added Dark Mode to project pages

---
+++ JS/Dark-Mode.js
@@ -0,0 +1,23 @@
+var darkModeOn = false;
+
+$("#dark-mode-button").click(function(){
+
+    if(darkModeOn == false){
+        $("*").css('background-color', '#0e0c1c');
+        $("#Headings").css('background-color', 'transparent');
+        $("#Header-Links").css('background-color', 'transparent');
+        $("li").css('background-color', 'transparent');
+        $("a").css('background-color', 'transparent');
+        darkModeOn = true;
+        $('#dark-mode-button').html('Turn Lights On');
+    } else {
+        $("*").css('background-color', '#536b99');
+        $("#Headings").css('background-color', 'transparent');
+        $("#Header-Links").css('background-color', 'transparent');
+        $("li").css('background-color', 'transparent');
+        $("a").css('background-color', 'transparent');
+        darkModeOn = false;
+        $('#dark-mode-button').html('Turn Lights Off');
+    };
+
+});
