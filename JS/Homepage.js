02251f1543272a1c4dc0110db36ec596fdfc81b2
alexvargas000 <75872850+alexvargas000@users.noreply.github.com>
2024-01-08 23:01:32 -0500

    Added and Finished Mobile Menu

--- JS/Homepage.js
+++ JS/Homepage.js
@@ -19,22 +19,3 @@ $("#Bloopers").mouseleave(function(){
 
     $(this).css('width', '50em');
 });
-
-// MOBILE MENU
-
-var menuOn = false;
-
-$("#Menu-Button").click(function(){
-
-    if (menuOn == false){
-    $("#Mobile-Menu").css('left', '0');
-    $("#Menu-Button").removeClass("Open-Mobile-Menu");
-    $("#Menu-Button").addClass("Close-Mobile-Menu");
-        menuOn = true;
-    } else {
-        $("#Mobile-Menu").css('left', '9.5em');
-        $("#Menu-Button").addClass("Open-Mobile-Menu");
-        $("#Menu-Button").removeClass("Close-Mobile-Menu");
-        menuOn = false;
-    }
-});
--- JS/Nav-Bar.js
+++ JS/Nav-Bar.js
@@ -15,3 +15,36 @@ worksNavHover.mouseleave(function(){
     worksNav.css('display', 'none');
 
 });
+
+// MOBILE MENU
+
+var menuOn = false;
+
+$("#Menu-Button").click(function(){
+
+    if (menuOn == false){
+    $("#Mobile-Menu").css('left', '0');
+    $("#Menu-Button").removeClass("Open-Mobile-Menu");
+    $("#Menu-Button").addClass("Close-Mobile-Menu");
+        menuOn = true;
+    } else {
+        $("#Mobile-Menu").css('left', '9.5em');
+        $("#Menu-Button").addClass("Open-Mobile-Menu");
+        $("#Menu-Button").removeClass("Close-Mobile-Menu");
+        menuOn = false;
+    }
+});
+
+var worksMenuOn = false;
+
+$("#Works-Menu-Mobile").click(function(){
+
+    if (worksMenuOn == false){
+    $(".Works-Menu-Item").css('display', 'block');
+        worksMenuOn = true;
+    } else {
+        $(".Works-Menu-Item").css('display', 'none');
+        worksMenuOn = false;
+    }
+
+});
