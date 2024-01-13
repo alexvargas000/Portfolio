1272d66872f2a672b6de54edb905732ee2575ba5
alexvargas000 <75872850+alexvargas000@users.noreply.github.com>
2024-01-12 21:44:14 -0500

    Update Dark-Mode.js

--- JS/Dark-Mode.js
+++ JS/Dark-Mode.js
@@ -3,21 +3,15 @@ var darkModeOn = false;
 $("#dark-mode-button").click(function(){
 
     if(darkModeOn == false){
+        $("*").css('transition', 'all .5s');
         $("*").css('background-color', '#0e0c1c');
-        $("*").css('transition', 'all 1s');
-        $("#Headings").css('background-color', 'transparent');
-        $("#Header-Links").css('background-color', 'transparent');
-        $("li").css('background-color', 'transparent');
-        $("a").css('background-color', 'transparent');
+        $("a, li, h1, #Header-Links, #Headings").css('background-color', 'transparent');
         $("p, h1, a, h2, button, li").css('opacity', '50%');
         darkModeOn = true;
         $('#dark-mode-button').html('Turn Lights On');
     } else {
         $("*").css('background-color', '#1f283a');
-        $("#Headings").css('background-color', 'transparent');
-        $("#Header-Links").css('background-color', 'transparent');
-        $("li").css('background-color', 'transparent');
-        $("a").css('background-color', 'transparent');
+        $("a, li, h1, #Header-Links, #Headings").css('background-color', 'transparent');
         $("p, h1, a, h2, button, li").css('opacity', '100%');
         darkModeOn = false;
         $('#dark-mode-button').html('Turn Lights Off');
