e6d5a780762de6969f0aabd0a02db7f38305d8a2
alexvargas000 <75872850+alexvargas000@users.noreply.github.com>
2024-01-16 04:04:48 -0500

    Update Image-Zoom-100-Days.js

--- JS/Image-Zoom-100-Days.js
+++ JS/Image-Zoom-100-Days.js
@@ -25,9 +25,9 @@ $(".project-pic").click(function(){
 
         $("#Image-Safety").css('display', 'block');
 
-    if(w <= 600){
-        $(this).css('width', '70%');
-    }
+        if(w <= 600){
+            $(this).css('width', '70%');
+        };
 
     } else {
             $(this).removeClass("Image-Zoom");
@@ -39,7 +39,7 @@ $(".project-pic").click(function(){
         if(w <= 600){
             $(this).css('width', '40%');
         }
-    }
+    };
 
 });
 
@@ -47,34 +47,35 @@ $(".poster-pic").click(function(){
 
     if (imageZoomed == false){
         $(this).addClass("Image-Zoom");
-        $(this).css('width', '40%');
         imageZoomed = true;
 
         $("#Image-Safety").css('display', 'block');
 
     if(w <= 600){
         $(this).css('width', '100%');
-    }
+    } else if(w >= 601){
+        $(this).css('width', '40%')
+    };
 
     } else {
             $(this).removeClass("Image-Zoom");
-            $(this).css('width', '25%');
             imageZoomed = false;
 
             $("#Image-Safety").css('display', 'none');
 
         if(w <= 600){
             $(this).css('width', '90%');
-        }
-    }
+        } else if(w >= 601){
+            $(this).css('width', '25%')
+        };
+
+    };
 
 });
 
 $("#Image-Safety").click(function(){
     $(".project-pic").removeClass("Image-Zoom");
     $(".poster-pic").removeClass("Image-Zoom");
-    $(".project-pic").css('width', '20%');
-    $(".poster-pic").css('width', '25%');
     imageZoomed = false;
 
     $("#Image-Safety").css('display', 'none');
@@ -82,6 +83,9 @@ $("#Image-Safety").click(function(){
     if(w <= 600){
         $(".project-pic").css('width', '40%');
         $(".poster-pic").css('width', '90%');
-    }
+    } else if(w >= 601){
+        $(".project-pic").css('width', '20%');
+        $(".poster-pic").css('width', '25%');
+    };
 
 });
