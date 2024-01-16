c64854746caac3a7c89c2392a842ef9bf0f7948b
alexvargas000 <75872850+alexvargas000@users.noreply.github.com>
2024-01-15 23:47:30 -0500

    Added Posters and Updated all names

--- JS/Image-Zoom-100-Days.js
+++ JS/Image-Zoom-100-Days.js
@@ -7,8 +7,10 @@ $(window).resize(function(){
 
     if(w <= 600){
         $(".project-pic").css('width', '40%');
+        $(".poster-pic").css('width', '100%');
     } else if(w >= 601){
         $(".project-pic").css('width', '20%');
+        $(".poster-pic").css('width', '25%');
     }
 
 
@@ -41,15 +43,45 @@ $(".project-pic").click(function(){
 
 });
 
+$(".poster-pic").click(function(){
+
+    if (imageZoomed == false){
+        $(this).addClass("Image-Zoom");
+        $(this).css('width', '40%');
+        imageZoomed = true;
+
+        $("#Image-Safety").css('display', 'block');
+
+    if(w <= 600){
+        $(this).css('width', '100%');
+    }
+
+    } else {
+            $(this).removeClass("Image-Zoom");
+            $(this).css('width', '25%');
+            imageZoomed = false;
+
+            $("#Image-Safety").css('display', 'none');
+
+        if(w <= 600){
+            $(this).css('width', '90%');
+        }
+    }
+
+});
+
 $("#Image-Safety").click(function(){
     $(".project-pic").removeClass("Image-Zoom");
+    $(".poster-pic").removeClass("Image-Zoom");
     $(".project-pic").css('width', '20%');
+    $(".poster-pic").css('width', '25%');
     imageZoomed = false;
 
     $("#Image-Safety").css('display', 'none');
 
     if(w <= 600){
         $(".project-pic").css('width', '40%');
+        $(".poster-pic").css('width', '90%');
     }
 
 });
